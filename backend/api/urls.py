824506594c547e04b76181fa8e71fdeb4547169e
Sundess <sundess17@gmail.com>
2024-08-19 15:13:29 +0545

    refactor: login view

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -2,10 +2,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("products/", views.ProductListCreate.as_view(), name= "product-list"),
-    path("products/delete/<int:pk>/", views.ProductDelete.as_view(), name="delete-product"),
+    path("products/", views.ProductListCreate.as_view(), name="product-list"),
+    path("products/delete/<int:pk>/",
+         views.ProductDelete.as_view(), name="delete-product"),
     path("register/", views.register_view, name="register"),
     path("login/", views.login_view, name="login"),
     path("logout/", views.logout_view, name="logout"),
     path("dashboard/", views.dashboard_view, name="dashboard"),
+    path("login2/", views.login_view2, name="login2"),
 ]
--- backend/api/views.py
+++ backend/api/views.py
@@ -2,41 +2,42 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 from rest_framework import generics
 from .serializers import UserSerializer, ProductSerializer
-from rest_framework.permissions import IsAuthenticated, AllowAny 
+from rest_framework.permissions import IsAuthenticated, AllowAny
 from .models import Product
 
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth import login, logout
 
+
 class ProductListCreate(generics.ListCreateAPIView):
     serializer_class = ProductSerializer
     permission_classes = [IsAuthenticated]
-    
+
     def get_queryset(self):
         user = self.request.user
-        return Product.objects.filter(created_by = user)
-    
+        return Product.objects.filter(created_by=user)
+
     def perform_create(self, serializer):
         if serializer.is_valid():
-            serializer.save(author = self.request.user)
+            serializer.save(author=self.request.user)
         else:
             print(serializer.errors)
-            
+
+
 class ProductDelete(generics.DestroyAPIView):
     queryset = Product.objects.all()
     permission_classes = [IsAuthenticated]
 
     def get_queryset(self):
         user = self.request.user
-        return Product.objects.filter(author = user)
-    
+        return Product.objects.filter(author=user)
+
 
 # Create your views here.
 class CreateUserView(generics.CreateAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
     permission_classes = [AllowAny]
-    
 
 
 def register_view(request):
@@ -44,24 +45,24 @@ def register_view(request):
         form = UserCreationForm(request.POST)
         if form.is_valid():
             user = form.save()
-            login(request,user)
+            login(request, user)
             return redirect('dashboard')
     else:
-        inital_data = {'username': '', 'password1' : '', 'password2': ''}
-        form = UserCreationForm(initial = inital_data)
+        inital_data = {'username': '', 'password1': '', 'password2': ''}
+        form = UserCreationForm(initial=inital_data)
     return render(request, 'auth/register.html', {'form': form})
 
 
 def login_view(request):
     if request.method == 'POST':
-        form = AuthenticationForm(request, data = request.POST)
+        form = AuthenticationForm(request, data=request.POST)
         if form.is_valid():
             user = form.get_user()
-            login(request,user)
+            login(request, user)
             return redirect('dashboard')
     else:
-        inital_data = {'username': '', 'password' : ''}
-        form = AuthenticationForm(initial = inital_data)
+        inital_data = {'username': '', 'password': ''}
+        form = AuthenticationForm(initial=inital_data)
     return render(request, 'auth/login.html', {'form': form})
 
 
@@ -69,5 +70,18 @@ def logout_view(request):
     logout(request)
     return redirect('login')
 
+
 def dashboard_view(request):
     return render(request, 'dashboard.html')
+
+
+def login_view2(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+
+        user = User.objects.create_user(username, password)
+        print(username, email, password)
+
+    return render(request, 'auth/login2.html')
